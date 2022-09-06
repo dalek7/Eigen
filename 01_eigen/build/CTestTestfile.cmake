d18ca933edbcc2cbf578b7183b24fea032c4de5e
seung@A6000 <seung@A6000>
2022-09-07 00:18:01 +0900

    sync

--- 01_eigen/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seung/repos-cpp/0_Fundamental/01_eigen
-# Build directory: /home/seung/repos-cpp/0_Fundamental/01_eigen/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
